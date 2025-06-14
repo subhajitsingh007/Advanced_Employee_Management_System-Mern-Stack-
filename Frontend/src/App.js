ef5590ca493fdad06bdaf9d764b81790fcdf88ed
Subhajit SIngh <94064293+subhajitsingh007@users.noreply.github.com>
2025-06-14 18:48:40 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,14 +6,18 @@ import LoginPage from './pages/Login'; // Login Page
 import SignupPage from './pages/Signup'; // Signup Page
 import About from './pages/About';
 import FAQ from './pages/FAQ'; // FAQ Page
-import AdminDashboardPage from './pages/AdminDashboardPage'; // Dashboard Page (Employee Management)
+import AdminDashboardPage from './pages/AdminDashboard.js'; // Dashboard Page (Employee Management)
 import PerformancePage from './pages/Performance'; // Performance Page (Employee Management) // Employee Page (Employee Management)
 import AttendancePage from './pages/Attendance'; // Attendance Page (Employee Management)
 import EmployeeManagement from './pages/EmployeeManagement'; // Employee Page (Employee Management)
+import AdminLeaveDashboard from './pages/AdminLeaveManagement'; // Admin Leave Management Page
 import PayrollPage from './pages/Payroll'; // Payroll Page (Employee Management)
 import Settings from './pages/SettingsPage'; // Settings Page (Employee Management)
 import Contact from './pages/Contact'
-
+import EmployeeDashboard from './pages/Employee Dashboard/EmployeeDashboard.js'; // Employee Dashboard
+import EmployeeAttendance from './pages/Employee Dashboard/EmployeeAttendance.js';
+import LeaveRequestPage from './pages/Employee Dashboard/LeaveRequests.js'; // Employee Attendance
+import Profile from './pages/Employee Dashboard/Profile.js'; // Employee Profile
 function App() {
   
   return (
@@ -34,9 +38,15 @@ function App() {
        <Route path="/performance" element={<PerformancePage />} />
         <Route path="/payroll" element={<PayrollPage />} />
         <Route path ="/employeemanagement" element={<EmployeeManagement />} />
+        <Route path="/leaves" element={<AdminLeaveDashboard />} />
         <Route path="/settings" element={<Settings />} />
 
         <Route path="/contact" element={<Contact />} />
+        {/* Employee Dashboard route */}
+        <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+        <Route path="/employee-attendance" element={<EmployeeAttendance />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/leave-requests" element={<LeaveRequestPage />} />
         
 
         {/* 404 Not Found route */}
